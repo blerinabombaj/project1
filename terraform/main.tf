10c6d0aaee90e0f6237c516d3e6a301e7bbb6094
Robert Lleshi <robertlleshi@gmail.com>
2026-03-25 20:04:09 +0100

    eks upgrade to 1.31

--- terraform/main.tf
+++ terraform/main.tf
@@ -3,7 +3,6 @@ module "eks" {
 
   region             = "eu-west-1"
   cluster_name       = "project1-cluster"
-  cluster_version    = "1.30"
   node_instance_type = "c7i-flex.large"
   node_desired_count = 1
   node_min_count     = 1

4373cca811308b0aa0a4c1500ff8640b6aed3607
Robert Lleshi <robertlleshi@gmail.com>
2026-03-25 19:08:21 +0100

    edit instance type on tf files

--- terraform/main.tf
+++ terraform/main.tf
@@ -4,7 +4,7 @@ module "eks" {
   region             = "eu-west-1"
   cluster_name       = "project1-cluster"
   cluster_version    = "1.29"
-  node_instance_type = "t2.large"
+  node_instance_type = "c7i-flex.large"
   node_desired_count = 1
   node_min_count     = 1
   node_max_count     = 1
